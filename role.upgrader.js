c7405fbedfeac0b613b826c968544bd84762936d
ZebraLan <davieloveguitar@hotmail.com>
2018-09-19 21:36:57 +0800

    try upgrader room switching behaviour

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,39 +2,60 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
+        if(creep.carry.energy == creep.carryCapacity &&
+            creep.room.controller.my &&
+            creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
             creep.say('⚡ upgrade');
-        }
-
-        if(creep.memory.upgrading) {
-            // creep.moveTo(Game.spawns['Spawn1']);
-            // return
-            // if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-            if(creep.upgradeController(Game.spawns['Spawn1'].room.controller)
-                == ERR_NOT_IN_RANGE && creep.room === Game.spawns['Spawn1'].room) {
-                creep.moveTo(
-                    Game.spawns['Spawn1'].room.controller,
-                    {
-                        visualizePathStyle: {stroke: '#ffffff'},
-                        maxRooms: creep.room === 1
-                    }
-               );
-                // creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
+            creep.moveByPath(
+                creep.room.findPath(
+                    creep.pos,
+                    creep.room.controller.pos))
         }
         else {
+            creep.say('🔄 harvest');
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
-};
+}
+
+
+
+
+//         if(creep.memory.upgrading && creep.carry.energy == 0) {
+//             creep.memory.upgrading = false;
+//             creep.say('🔄 harvest');
+//         }
+//         if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+//             creep.memory.upgrading = true;
+//             creep.say('⚡ upgrade');
+//         }
+// 
+//         if(creep.memory.upgrading) {
+//             // creep.moveTo(Game.spawns['Spawn1']);
+//             // return
+//             // if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+//             if(creep.upgradeController(Game.spawns['Spawn1'].room.controller)
+//                 == ERR_NOT_IN_RANGE && creep.room === Game.spawns['Spawn1'].room) {
+//                 creep.moveTo(
+//                     Game.spawns['Spawn1'].room.controller,
+//                     {
+//                         visualizePathStyle: {stroke: '#ffffff'},
+//                         maxRooms: creep.room === 1
+//                     }
+//                );
+//                 // creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+//             }
+//         }
+//         else {
+//             var sources = creep.room.find(FIND_SOURCES);
+//             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+//                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+//             }
+//         }
+//     }
+// };
 
 module.exports = roleUpgrader;
