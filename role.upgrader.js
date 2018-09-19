c3284598dbcb4e358c5bda8e5f7213601b54bd28
ZebraLan <davieloveguitar@hotmail.com>
2018-09-20 01:54:10 +0800

    try using cross room search to overcome previous pitfall

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,91 +1,38 @@
 var roleUpgrader = {
-
     /** @param {Creep} creep **/
     run: function(creep) {
-        if((creep.pos.x in [0, 49] || creep.pos.y in [0, 49]) &&
-            creep.fatigue == 0
-        ) {
-            creep.move(
-                creep.pos.y == 0  ? UP    :
-                creep.pos.x == 49 ? RIGHT :
-                creep.pos.y == 49 ? DOWN  :
-                LEFT)
-            return
-        }
-
         if(creep.carry.energy == creep.carryCapacity &&
-            creep.room.controller instanceof OwnedStructure &&
-            creep.room.controller.my &&
-            creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE  &&
+            (!(creep.room.controller instanceof OwnedStructure) ||
+                !creep.controller.my ||
+                creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) &&
             creep.fatigue == 0
         ) {
             creep.say('⚡ upgrade');
-            creep.moveTo(creep.room.controller);
-            // creep.moveByPath(
-            //     PathFinder.search(
-            //         creep.pos,
-            //         {pos: creep.room.controller.pos, range: 1},
-            //         {maxRooms: 1}))
-                // creep.room.findPath(
-                //     creep.pos,
-                //     creep.room.controller.pos))
+            creep.moveTo(
+                [
+                    creep.room.controller,
+                    ..._.values(
+                        Game.map.describeExits(creep.room.name)
+                    ).map((name) => Game.rooms[name].controller)
+                ].find((controller) => controller instanceof OwnedStructure && controller.my))
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES);
+            var sources = [
+                creep.room,
+                ..._.values(
+                    Game.map.describeExits(creep.room.name)
+                ).map((name) => Game.rooms[name])
+            ].find((room) => (
+                (controller) => controller instanceof OwnedStructure && controller.my
+            )(room.controller)).find(FIND_SOURCES)
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE &&
                 creep.fatigue == 0
             ) {
                 creep.say('🔄 harvest');
                 creep.moveTo(sources[0]);
-                // creep.moveByPath(
-                //     PathFinder.search(
-                //         creep.pos,
-                //         {pos: sources[0].pos, range: 1},
-                //         {maxRooms: 1}))
-                    // creep.room.findPath(
-                    //     creep.pos,
-                    //     sources[0].pos))
-                // creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
 }
-
-
-
-
-//         if(creep.memory.upgrading && creep.carry.energy == 0) {
-//             creep.memory.upgrading = false;
-//             creep.say('🔄 harvest');
-//         }
-//         if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-//             creep.memory.upgrading = true;
-//             creep.say('⚡ upgrade');
-//         }
-// 
-//         if(creep.memory.upgrading) {
-//             // creep.moveTo(Game.spawns['Spawn1']);
-//             // return
-//             // if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-//             if(creep.upgradeController(Game.spawns['Spawn1'].room.controller)
-//                 == ERR_NOT_IN_RANGE && creep.room === Game.spawns['Spawn1'].room) {
-//                 creep.moveTo(
-//                     Game.spawns['Spawn1'].room.controller,
-//                     {
-//                         visualizePathStyle: {stroke: '#ffffff'},
-//                         maxRooms: creep.room === 1
-//                     }
-//                );
-//                 // creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-//             }
-//         }
-//         else {
-//             var sources = creep.room.find(FIND_SOURCES);
-//             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-//                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-//             }
-//         }
-//     }
-// };
 
 module.exports = roleUpgrader;
